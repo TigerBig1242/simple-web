3c45b5175dcc883f428d3e86020e78c0141cf696
tigerbig <thammaphop1230@gmail.com>
2026-05-14 11:01:22 +0700

    Fix code site and base github.io at base from /simple-web to /simple-web/

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tigerbig1242.github.io',
-  base: '/simple-web',
+  base: '/simple-web/',
   integrations: [
     react(),
   ],
